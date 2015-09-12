60d6c3cd66f3a3f3e3f6ae862bfe6b35753a806b
ryoogata <ryo.ogata@gmail.com>
2015-09-12 11:48:11 +0900

    first commit

---
+++ chaos.r
@@ -0,0 +1,16 @@
+x <- 0.120000
+i <- 1
+vectX <- c()
+vectY <- c()
+
+while ( i <= 100)
+{
+  y <- 4 * x * ( 1 - x)
+  vectX[[i]] <- i
+  vectY[[i]] <- y
+  x <- y
+  i <- i + 1
+}
+print(y)
+theDF <- data.frame(vectX, vectY)
+ggplot(theDF, aes(x = vectX, y = vectY)) + geom_line()
